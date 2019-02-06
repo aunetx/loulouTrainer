6e9a657ec26253f7a926b4899f39dc42bd2bf550
Aurélien Hamy <aunetx@users.noreply.github.com>
2019-02-06 19:18:31 +0100

    Version 0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # loulouTrainer
+
+GUI application to train Loulou, a simple neural network built for MNIST database.
+Created for educationals and tests reasons, but feel free to adapt it however you want.
+* Version 0.0.1 : Only a basic electron application.
---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron'),
+      sass = require('sass'),
+      fs = require('fs');
+
+function initCss(pathToSass, pathToCss) {
+  sass.render({file: pathToSass}, function(err, result) {
+    if (!err) {
+      fs.writeFile(pathToCss, result.css, (err) => {
+        if (!err) {
+          console.log('Compilation of ' + pathToSass + ' done successfully.')
+        }
+      });
+    } else if (err) {
+      console.log(err);
+    }
+  });
+}
+
+function createWindow () {
+  win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadFile('ressources/html/index.html');
+}
+
+initCss('ressources/sass/main.scss', 'ressources/css/style.css');
+app.on('ready', createWindow);
